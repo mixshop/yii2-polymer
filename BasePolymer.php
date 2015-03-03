fb9d4c5b9fd9e69e12143fafc2ee878875f68c5d
mixshop <gj-guojing@hotmail.com>
2015-03-03 07:46:33 -0500

    Initial

---
+++ BasePolymer.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2008 Yii Software LLC
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\polymer;
+
+
+/**
+ * Polymer implements most commonly used image manipulation functions using the [Polymer library](http://www.polymer-project.org/).
+ *
+ * @author Jing Guo <jing@mixshop.com>
+ * @since 2.0
+ */
+class BasePolymer
+{
+}
---
+++ Polymer.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * @link http://www.yiiframework.com/
+ * @copyright Copyright (c) 2014 Mixshop Inc
+ * @license http://www.yiiframework.com/license/
+ */
+
+namespace yii\polymer;
+
+/**
+ * Polymer implements most commonly used image manipulation functions using the [Polymer library](http://www.polymer-project.org/).
+ *
+ * @author Jing Guo <jing@mixshop.com>
+ * @since 2.0
+ */
+class Polymer extends BasePolymer
+{
+}
